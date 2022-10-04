8864ad93972548ab6762c04289a2c3dd476b4345
Yoselin17 <yoselin_yarg@hotmail.com>
2022-10-04 02:49:47 +0000

    listo

--- src/test_solution.py
+++ src/test_solution.py
@@ -9,8 +9,6 @@ def client():
         app.config['TESTING'] = True
 
         with app.test_client() as client:
-            # with app.app_context():
-            #     app.init_db()
             yield client
 
         os.close(db_fd)
